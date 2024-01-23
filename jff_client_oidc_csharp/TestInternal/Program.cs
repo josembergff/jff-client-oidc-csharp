34426aa44bffe0a9acd10a13ce1f4bbe9ce297f8
Josemberg Ferreira <josembergff@gmail.com>
2024-01-23 10:30:24 -0300

    Ajuste test e build info

--- jff_client_oidc_csharp/TestInternal/Program.cs
+++ jff_client_oidc_csharp/TestInternal/Program.cs
@@ -1,20 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-using jff_client_oidc_csharp;
+﻿using jff_client_oidc_csharp;
+using jff_client_oidc_csharp_legacy;
 
 Console.WriteLine("Hello, World!");
 
-var client = new ClientCredentials("https://localhost:62862", "api", "secret", new string[] { "openid" });
+testeClientLegacy();
 
-var token = client.GetToken().Result;
-
-if (token.Success)
+void testeClientLegacy()
 {
-    var resultAPI = client.Get<dynamic>("https://localhost:3000").Result;
+    var client = new ClientCredentialsLegacy("https://localhost:62862", "api", "secret", new string[] { "openid" });
 
-    Console.WriteLine(resultAPI.Result.ToString());
+    var token = client.GetToken().Result;
 }
-else
+
+void testeClient()
 {
-    Console.WriteLine(token.Error);
-    Console.WriteLine(string.Join(";", token.ListErrors));
+    var client = new ClientCredentials("https://localhost:62862", "api", "secret", new string[] { "openid" });
+
+    var token = client.GetToken().Result;
+
+    if (token.Success)
+    {
+        var resultAPI = client.Get<dynamic>("https://localhost:3000").Result;
+
+        Console.WriteLine(resultAPI.Result.ToString());
+    }
+    else
+    {
+        Console.WriteLine(token.Error);
+        Console.WriteLine(string.Join(";", token.ListErrors));
+    }
 }
